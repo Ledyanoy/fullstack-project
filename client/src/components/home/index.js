2dca6020d2c896c24b348c63b31b4a5cba84ec1d
Коренев Александр <akorenev@local.htc-cs.com>
2021-02-19 16:00:24 +0400

    add initial response for articles

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, {useEffect, useReducer} from 'react'
 import {Grid} from '@material-ui/core'
 import ArticleCard from "../../utils/articleCard";
 
+import {useDispatch, useSelector} from 'react-redux';
+import {getArticlesTC} from '../../store/actions/article_actions'
+
+const initialState = {
+    sortBy: "_id",
+    order: "desc",
+    limit: 8,
+    skip: 0
+}
+
+const reducer = (state, newState ) => {
+    return {...state, ...newState}
+}
+
 
 const Home = () => {
+    const articles = useSelector(state => state.articles)
+    const dispatch = useDispatch()
+
+
+    useEffect(()=> {
+        if (articles && !articles.articles) {
+            dispatch(getArticlesTC(initialState))
+        }
+    }, [dispatch, articles])
+
+
     return (
         <div className='mt-5'>
             <div>CARROUSEL</div>
@@ -23,8 +48,6 @@ const Home = () => {
                 <Grid key={1} item xs={12} sm={6} lg={3}>
                     <ArticleCard/>
                 </Grid>
-
-
             </Grid>
         </div>
     )
---
+++ client/src/store/actions/article_actions.js
@@ -0,0 +1,16 @@
+import * as articles from './index'
+import axios from 'axios'
+
+axios.defaults.headers.post['Content-Type'] = 'application/json'
+
+export const getArticlesTC = (sort) => {
+    return async (dispatch, getState) => {
+        try {
+            const arts = await axios.post('/api/articles/loadmore', sort)
+            dispatch(articles.getArticles(arts.data))
+
+        } catch (error) {
+
+        }
+    }
+}
